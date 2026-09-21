add0e32fc90ac9ad5b689b2d9cce611052d09517
iJuker <163083454+kosaligtech@users.noreply.github.com>
2026-09-21 08:47:41 +0700

    fix error

--- src/composable/bridge-getway/useBuy.ts
+++ src/composable/bridge-getway/useBuy.ts
@@ -1,11 +1,7 @@
-import { onMounted } from "vue";
+import { onBeforeMount, onMounted } from "vue";
 import { storeToRefs } from "pinia";
 import { useInitBridge } from "./useInitBridge";
-import {
-  useStorePayment,
-  type IPaymentCompletedData,
-  type IPaymentFailedData,
-} from "@/composable/state/useStorePayment";
+import { useStorePayment, type IPaymentCompletedData, type IPaymentFailedData } from "@/composable/state/useStorePayment";
 
 /**
  * registerHandler:buyNowCallBack
@@ -16,13 +12,18 @@ export const useBuy = () => {
   const storePayment = useStorePayment();
   const { isSuccess, isFailed, paymentCompleted, paymentFailed } = storeToRefs(storePayment);
 
+  onBeforeMount(() => {
+    storePayment.setPaymentCompleted(null);
+    storePayment.setPaymentFailed(null);
+  });
+
   onMounted(() => {
     bridge.registerHandler("payment.completed", (data: IPaymentCompletedData) => {
-      storePayment.setPaymentCompleted(data);
+      storePayment.setPaymentCompleted(data ?? {});
     });
 
     bridge.registerHandler("payment.failed", (data: IPaymentFailedData) => {
-      storePayment.setPaymentFailed(data);
+      storePayment.setPaymentFailed(data ?? {});
     });
   });
 
@@ -31,7 +32,7 @@ export const useBuy = () => {
     const requestId = `PAY-${Date.now()}-${Math.floor(Math.random() * 1000000)
       .toString()
       .padStart(6, "0")}`;
-    bridge.callHandler("payment.requested", { amount, currency:'USD', requestId, reference });
+    bridge.callHandler("payment.requested", { amount, currency: "USD", requestId, reference });
   };
 
   return { isSuccess, isFailed, paymentCompleted, paymentFailed, onBuyNow };
